2a84d58b93730f06b64820e52224ac16029f2357
seeamkhan <zafi005@gmail.com>
2016-09-24 00:40:17 +0600

    from linux

--- ex18.py
+++ ex18.py
@@ -3,13 +3,16 @@ def print_two(*args):
     arg1, arg2 = args
     print "arg1: %r, arg2: %r" % (arg1, arg2)
 
+
 # ok, that *args is actually pointless, we can just do this
 def print_two_again(arg1, arg2):
     print "arg1: %r, arg2: %r" % (arg1, arg2)
 
-#this just take one argument
+
+# this just take one argument
 def print_one(arg1):
     print "arg1: %r" % arg1
+
 
 # this one takes no argumnet
 def print_none():
